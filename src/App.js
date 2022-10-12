4049359904aedde070b1af5e6a398cc9c1643e51
Herberth Andrade <andrade.engmecanica@gmail.com>
2022-10-12 14:30:22 -0300

    only theory

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [reverse, setReverse] = useState(false);
+  const [counter, setCounter] = useState(0);
+  const reverseClass = reverse ? 'reverse' : '';
+
+  // componentDidUpdate => execulta toda vez que o component atualiza
+
+  useEffect(() => {
+    console.log('componentDidUpdate');
+  });
+
+  // componentDidMount => execulta 1 x
+  useEffect(() => {
+    console.log('componentDidMount');
+  }, []);
+
+  // Com dependência => execulta toda vez que a dependência mudar
+  useEffect(() => {
+    console.log('Contador mudou para: ', counter);
+  }, [counter]);
+
+  const handleClick = () => {
+    setReverse((reverse) => !reverse);
+  };
+
+  const handleIncrement = () => {
+    setCounter((c) => c + 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+
+        <h1>Counter: {counter}</h1>
+
+        <button type="button" onClick={handleIncrement}>
+          Increment {counter}
+        </button>
+        <br />
+
+        <button type="button" onClick={handleClick}>
+          Reverse {reverseClass}
+        </button>
       </header>
     </div>
   );
